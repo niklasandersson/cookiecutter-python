a66e8791a80a7b4dc8d0d4d2bdd728ae4ee76f1f
Niklas Andersson <me@niklasandersson.net>
2017-02-23 17:11:25 +0100

    Add basic config

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hej',
-    version='0.1',
+    name='{{cookiecutter.repo_name}}',
+    version='{{cookiecutter.version}}',
+    url='{{cookiecutter.github_url}}',
+    author='{{cookiecutter.full_name}}',
+    author_email='{{cookiecutter.email}}',
+    description='{{cookiecutter.description}}',
     packages=find_packages(),
     include_package_data=True,
     entry_points='''
         [console_scripts]
-        main={{cookiecutter.repo_name}}.main:main
+        {{cookiecutter.repo_name}}={{cookiecutter.repo_name}}.main:main
     '''
 )
